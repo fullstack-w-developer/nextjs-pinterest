9ef4922695359617dc7fffc0e081196faa4bf180
mahdi-shariflo <mahdilove279@gmail.com>
2022-06-13 09:27:58 +0430

    add image user

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
                 />
               </a>
             </Link>
-            <p className="hidden mg:block">{user.email}</p>
+            <p className="hidden md:block">{user.email}</p>
             <button
               onClick={() => SignOut()}
               className="text-gray-900 font-medium px-3 py-1 rounded-lg hover:bg-[#0096f5] hover:text-white transition-all "
